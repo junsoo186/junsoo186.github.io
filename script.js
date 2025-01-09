cd14277884bf593bc0d05095032c0f1e30ad6efe
junsoo <junsoo186@gmail.com>
2025-01-09 15:27:36 +0900

    연습

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function checkPassword() {
+    const password = document.getElementById('password').value;
+    const correctPassword = '아인즈울고운에 영광을';
+    
+    if (password === correctPassword) {
+        document.getElementById('login-screen').style.display = 'none';
+        document.getElementById('blog-content').style.display = 'block';
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+    }
+}
